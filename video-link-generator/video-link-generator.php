0357951a0aacdc2cc174937bb77096418a220091
gitomwtyczka <tomasz.brzozowski@impresjapr.pl>
2025-06-19 01:16:28 +0200

    Add files via upload

---
+++ video-link-generator/video-link-generator.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Video Link Generator
+Plugin URI: https://wordpress.org/plugins/video-link-generator/
+Description: Darmowa wersja wtyczki do automatycznego tworzenia stron z filmami.
+Version: 1.0.0
+Author: Twoje Imię
+License: GPL2
+*/
+
+defined('ABSPATH') or die('No script kiddies please!');
+
+define('VLG_FREE_ACTIVE', true);
+
+add_action('admin_menu', function() {
+    add_options_page('Video Link Generator', 'Video Link Generator', 'manage_options', 'video-link-generator', function() {
+        echo '<div class="wrap"><h2>Video Link Generator (Free)</h2><p>Działa wtyczka darmowa.</p></div>';
+    });
+});
